f13931e89a2b308adab8a3518696bea0c79b1521
Bartosz Golczewski <golczewski@74.gmail.com>
2025-01-10 13:05:43 +0100

    dodane magiczne linijki

--- main.cpp
+++ main.cpp
@@ -137,6 +137,10 @@ struct MaxHeap{
 };
 
 int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+
     int n, o, price, tax, extra_price;
     char flag;
     std::cin >> n;
